6eaf821cfe3c8be696553669af468b5798cee4ba
David Maza <david.maza@hotmail.es>
2022-09-26 11:49:58 +0200

    commit

--- src/components/HeaderLayout.vue
+++ src/components/HeaderLayout.vue
@@ -9,9 +9,9 @@
         <strong>PC3-3 David Maza</strong>
       </a>
       <div class="btn-group" role="group" aria-label="Default button group">
-        <a href="https://www.linkedin.com/in/davidmaza76/"><button type="button" class="btn btn-outline-light mx-1">linkedin</button></a>
+        <a href="https://www.linkedin.com/in/davidmaza76/"><button type="button" class="btn btn-outline-light mx-1">Linkedin</button></a>
         <a href="https://github.com/d-maza"><button type="button" class="btn btn-outline-light mx-1">GitHub</button></a>
-        <a href="/"><button type="button" class="btn btn-outline-light mx-1">About</button></a>
+        <a href="https://github.com/d-maza/PC3-3-VUE/archive/refs/heads/main.zip"><button type="button" class="btn btn-outline-light mx-1">Descargar</button></a>
 
       </div>
   
